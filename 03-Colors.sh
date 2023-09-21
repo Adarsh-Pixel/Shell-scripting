9fa593f9ee2bd066eb86c064ea8df60d8865beb3
Adarsh <kumaradarsh921@gmail.com>
2023-09-21 20:10:09 +0530

    Autopush

--- 03-Colors.sh
+++ 03-Colors.sh
@@ -1,4 +1,4 @@
 echo -e "\e[31m I am printing RED color \e[0m"
 echo -e "\e[34m I am printing Blue color \e[0m"
 
-echo -e "\e[103:31m This is two color message \e[0m"
+echo -e "\e[103;31m This is two color message \e[0m"

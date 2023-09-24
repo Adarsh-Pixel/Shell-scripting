e625a1e3f4cd668936884bcb4dc3a4b27040c40a
Adarsh <kumaradarsh921@gmail.com>
2023-09-24 17:41:30 +0530

    Autopush

--- 05-dynamic-vars.sh
+++ 05-dynamic-vars.sh
@@ -4,5 +4,5 @@
 DATE="$(date +%F)"
 SESSIONS_COUNT=$(who | wc -l)
 
-echo -e "\e[32 Today's date is $DATE \e[0m"
-echo "total number of action sessions $SESSIONS_COUNT"
+echo -e "Today's date is \e[32m $DATE \e[0m"
+echo -e "total number of action sessions \e[32m $SESSIONS_COUNT \e[0m"

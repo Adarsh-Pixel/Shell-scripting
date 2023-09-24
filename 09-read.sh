6ccd0f8f4f381dd928cf238cceda15a57b3f4586
Adarsh <kumaradarsh921@gmail.com>
2023-09-24 18:17:05 +0530

    Autopush

--- 09-read.sh
+++ 09-read.sh
@@ -3,3 +3,5 @@
 read -p "enter your name:" name
 
 echo -e "Name of the person who ran the script is $name"
+
+#this is used for promt users to enter
---
+++ 10-commands.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+There are four types of commands available: 
+
+# Binary (#bin ...../sbin)
+# aliases
+# Shell built-in commands
+# Funtions
+
+# To declar a funtion
+
+f() {
+    echo Hai
+}
+f 

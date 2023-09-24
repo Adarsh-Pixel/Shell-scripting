c412af698025716a3bc84e6dcef2450e2edab99c
Adarsh <kumaradarsh921@gmail.com>
2023-09-24 17:55:16 +0530

    Autopush

--- 06-specialvariables.sh
+++ 06-specialvariables.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
+a=10
+b=20
+c=30
 echo $0
 # special variables gives special properties to your variables
 
 echo "Name of the recently launched rocket in India is $1"
 echo "The expensive car in India is $2"
 echo "The fastest runner in the world is $3"
+
+echo $*
+echo $@
+echo $$
+echo $#
+echo $?

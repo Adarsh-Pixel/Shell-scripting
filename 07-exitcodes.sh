84d379ba19af5ccca064809ec384af8509a50e57
Adarsh <kumaradarsh921@gmail.com>
2023-09-27 16:32:52 +0530

    Autopush

--- 07-exitcodes.sh
+++ 07-exitcodes.sh
@@ -2,3 +2,5 @@
 
 # In Linux each and every action will have a exit code 
 #0-255 is the range of codes
+# among all 0, represents action completed successfully
+#Anything in between 1 to 255 represents either partial success, partial failure or complete failure
---
+++ 11-Redirectors.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#Redirectors are of 2 types in Bash
+#Input Redirectors (Means take input from a file)            :  <    (eg: Sudo mysql </tmp/student.sql)
+#output Redirectors (Means routing the Output to a file)      : >     (>> appends the latest output to an existing content)
+
+# #Outputs
+#     1) Standard Output                      > or >> or 1>
+#     2) Standard Error                       2> or 2>>
+#     3) Standard Output & Standard Error     &> or &>>
+
+
+# ls -ltr     > output.txt    #Redirects the output to output.txt
+# ls -ltr     >> output.txt   #Redirects & appends the output to output.txt
+# ls -ltr     2> output.txt   #Redirects the error only to output.txt
+# ls -ltr     &> output.txt   #Redirects the output or error to output.txt

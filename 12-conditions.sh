906d0e37e58671b5168661fae9ec64db9b12278c
Adarsh <kumaradarsh921@gmail.com>
2023-09-27 17:05:36 +0530

    Autopush

--- 12-conditions.sh
+++ 12-conditions.sh
@@ -13,14 +13,19 @@ Action=$1
 case $Action in
     start)
         echo -e "\e[32m Starting payment Service \e[0m"
+        exit 0
         ;;
     stop)
         echo -e "\e[31mStopping Payment Service \e[0m"
+        exit 1
         ;;
     restart)
         echo -e "\e[33mRestarting Payment Service \e[0m"
+        exit 2
         ;;
     *)
         echo -e "\e[33mValid options are start or stop or restart \e{0m"
         echo -e "example usage: \n \tbash Scriptname stop"
+        exit 3
+        ;;
 esac

d1314fb593d56b25e5a2885e3f8416adce55a3b8
Adarsh <kumaradarsh921@gmail.com>
2023-09-27 16:41:34 +0530

    Autopush

--- 07-exitcodes.sh
+++ 07-exitcodes.sh
@@ -4,3 +4,9 @@
 #0-255 is the range of codes
 # among all 0, represents action completed successfully
 #Anything in between 1 to 255 represents either partial success, partial failure or complete failure
+
+# 0         : Global success
+# 1-125     : some failure from the command
+# 125+      : system failure
+
+# Exit codes play a key role in debugging big scripts

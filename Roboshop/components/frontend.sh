2ac277d6e93a02e3534dca7d03477f6c6a2545b5
Adarsh <kumaradarsh921@gmail.com>
2023-09-28 16:45:42 +0530

    Autopush

--- Roboshop/components/frontend.sh
+++ Roboshop/components/frontend.sh
@@ -1,3 +1,35 @@
 #!/bin/bash
 
-echo "I am frontend"
+# validate the user who is running the script is a root user or not.
+
+USER_ID=$(id -u)
+
+if [ $USER_ID -ne 0 ] ; then
+    echo -e "\e[31m Script is expected to be executed by the root user or with a sudo privilage \e[0m \n \t Example: \n\t\t sudo bash wrapper.sh frontend"
+    exit 1
+fi
+
+echo -e "\e[35m Congiguring frontend.....! \e[0m \n"
+
+echo -e "Installing frontend :"
+yum install nginx -y &>> /tmp/frontend.log
+
+if [ $? -eq 0 ]; then
+    echo -e "\e[32m success \e[0m"
+else
+    echo -e "\e[31m failure \e[0m"
+fi 
+
+# yum install nginx -y
+# systemctl enable nginx
+# systemctl start nginx
+
+# curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
+
+# cd /usr/share/nginx/html
+# rm -rf *
+# unzip /tmp/frontend.zip
+# mv frontend-main/* .
+# mv static/* .
+# rm -rf frontend-main README.md
+# mv localhost.conf /etc/nginx/default.d/roboshop.conf

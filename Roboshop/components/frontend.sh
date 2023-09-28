a06b7958d79723fcd5562fad7b137c97cdecd419
Adarsh <kumaradarsh921@gmail.com>
2023-09-28 16:52:57 +0530

    Autopush

--- Roboshop/components/frontend.sh
+++ Roboshop/components/frontend.sh
@@ -12,7 +12,7 @@ fi
 echo -e "\e[35m Congiguring frontend.....! \e[0m \n"
 
 echo -n "Installing frontend :"
-yum install dgdfdf -y &>> /tmp/frontend.log
+yum install nginx -y &>> /tmp/frontend.log
 
 if [ $? -eq 0 ]; then
     echo -e "\e[32m success \e[0m"

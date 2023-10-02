9bfed9c61cf7a889d45e98656433a0a4c71957d4
Adarsh <kumaradarsh921@gmail.com>
2023-10-02 17:27:28 +0530

    Autopush

--- Roboshop/components/mongoDB.sh
+++ Roboshop/components/mongoDB.sh
@@ -1,3 +1,43 @@
 #!/bin/bash
 
 echo "I am mongoDB"
+
+USER_ID=$(id -u)
+COMPONENT=mongodb
+LOGFILE="/tmp/${COMPONENT}.log"
+
+if [ $USER_ID -ne 0 ] ; then
+    echo -e "\e[31m Script is expected to be executed by the root user or with a sudo privilage \e[0m \n \t Example: \n\t\t sudo bash wrapper.sh frontend"
+    exit 1
+fi
+
+stat () {
+    if [ $1 -eq 0 ]; then
+    echo -e "\e[32m success \e[0m"
+else
+    echo -e "\e[31m failure \e[0m"
+    fi 
+}
+
+echo -e "\e[35m Congiguring ${COMPONENT}.....! \e[0m \n"
+
+echo -n "configuring ${COMPONENT} repo : "
+curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/stans-robot-project/mongodb/main/mongo.repo
+stat $?
+
+echo -n "installing ${COMPONENT}  :"
+yum install -y mongodb-org      &>> ${LOGFILE}
+systemctl enable mongod         &>> ${LOGFILE}
+systemctl start mongod          &>> ${LOGFILE}
+stat $?
+
+
+# systemctl restart mongod
+
+# curl -s -L -o /tmp/mongodb.zip "https://github.com/stans-robot-project/mongodb/archive/main.zip"
+
+# cd /tmp
+# unzip mongodb.zip
+# cd mongodb-main
+# mongo < catalogue.js
+# mongo < users.js

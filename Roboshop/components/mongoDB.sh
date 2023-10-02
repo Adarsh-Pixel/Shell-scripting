e68b251a8447d4a6317ad8a940c42bdb3f2c4e42
Adarsh <kumaradarsh921@gmail.com>
2023-10-02 17:57:06 +0530

    Autopush

--- Roboshop/components/mongoDB.sh
+++ Roboshop/components/mongoDB.sh
@@ -35,7 +35,7 @@ systemctl start mongod          &>> ${LOGFILE}
 stat $?
 
 echo -n "enabling the ${COMPONENT} visibility :"
-sed -ie 's/127.0.0.1/0.0.0.0/g' mongodb.conf
+sed -ie 's/127.0.0.1/0.0.0.0/g' mongod.conf
 stat $?
 
 # systemctl restart mongod

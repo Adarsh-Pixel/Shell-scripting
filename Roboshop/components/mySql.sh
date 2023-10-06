57ce6e718fea946871ea511cc41b3b638bab618e
Adarsh <kumaradarsh921@gmail.com>
2023-10-06 18:19:13 +0530

    Autopush

--- Roboshop/components/mySql.sh
+++ Roboshop/components/mySql.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
-echo "I am mySql"
+COMPONENT=mysql
+
+source components/common.sh
+
+echo -e "\e[35m Congiguring ${COMPONENT}.....! \e[0m \n"
+
+echo -n "configuring ${COMPONENT} repo : "
+curl -s -L -o /etc/yum.repos.d/mysql.repo https://raw.githubusercontent.com/stans-robot-project/mysql/main/mysql.repo
+stat $?
+
+echo -n "installing ${COMPONENT}  :"
+yum install mysql-community-server -y     &>> ${LOGFILE}
+stat $?
+

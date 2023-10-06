8c3fc85bd8390b02f5e28dc0af14ca9b8f657354
Adarsh <kumaradarsh921@gmail.com>
2023-10-06 19:10:57 +0530

    Autopush

--- Roboshop/components/mySql.sh
+++ Roboshop/components/mySql.sh
@@ -19,3 +19,11 @@ echo -n "Starting ${COMPONENT}"
 systemctl enable mysqld  &>> ${LOGFILE}
 systemctl start mysqld   &>> ${LOGFILE}
 stat $?
+
+echo -n "Extracting the default mysql root password :"
+DEFAULT_ROOT_PASSWORD=grep "temporary password"  /var/log/mysqld.log | gawk -F : '{print $NF}'
+stat $?
+
+echo -n "Performing default password reset of root action:"
+echo "ALTER USER 'root'@'localhost' IDENTIFIED BY 'RoboShop@1'" | mysql --connect-expired-password -uroot -p$DEFAULT_ROOT_PASSWORD &>> ${LOGFILE}
+stat $?

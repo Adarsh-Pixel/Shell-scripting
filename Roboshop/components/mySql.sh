266348f5816c3ab399479fc9aee13361eaae3a34
Adarsh <kumaradarsh921@gmail.com>
2023-10-07 15:35:44 +0530

    Autopush

--- Roboshop/components/mySql.sh
+++ Roboshop/components/mySql.sh
@@ -21,7 +21,7 @@ systemctl start mysqld   &>> ${LOGFILE}
 stat $?
 
 echo -n "Extracting the default mysql root password :"
-DEFAULT_ROOT_PASSWORD=$(grep "temporary password"  /var/log/mysqld.log | awk -F : '{print $NF}')
+DEFAULT_ROOT_PASSWORD=$(grep 'temporary password'  /var/log/mysqld.log | awk -F " " '{print $NF}')
 stat $?
 
 #This should happen only once and that too for the first time, when it runs for the second time the jobs fail
@@ -29,6 +29,6 @@ stat $?
 echo "show databases;" | mysql -uroot -pRoboShop@1 &>> ${LOGFILE}
 if [ $? -ne 0 ]; then 
         echo -n "Performing default password reset of root action:"
-        echo "ALTER USER 'root'@'localhost' IDENTIFIED BY 'RoboShop@1'" | mysql --connect-expired-password -uroot -p$DEFAULT_ROOT_PASSWORD &>> ${LOGFILE}
+        echo "ALTER USER 'root'@'localhost' IDENTIFIED BY 'RoboShop@1'" | mysql connect-expired-password -uroot -p$DEFAULT_ROOT_PASSWORD &>> ${LOGFILE}
         stat $?
 fi 

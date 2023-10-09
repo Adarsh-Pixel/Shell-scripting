f5352f30e82c92a29f8fd0e87c8353426b6cdb35
Adarsh <kumaradarsh921@gmail.com>
2023-10-09 22:25:25 +0530

    Autopush

--- Roboshop/components/common.sh
+++ Roboshop/components/common.sh
@@ -122,8 +122,16 @@ PYTHON() {
 
         DOWNLOAD_AND_EXTRACT
 
-echo -n "Generating the artifacts :"
-cd /home/${APPUSER}/${COMPONENT}/
-pip3 install -r requirements.txt       &>> ${LOGFILE} 
-stat $?
+        echo -n "Generating the artifacts :"
+        cd /home/${APPUSER}/${COMPONENT}/
+        pip3 install -r requirements.txt       &>> ${LOGFILE} 
+        stat $?
+
+        USERID=$(id -u roboshop)
+        GROUPID=$(id -g roboshop)
+
+        echo -n "Updating the uid and gid in the ${COMPONENT}.ini file"
+        sed -i -e "/^uid/ c uid=$(USERID)" -e "/^gid/ c gid=$(GROUPID)" /home/${APPUSER}/${COMPONENT}/${COMPONENT}.ini 
+
+        CONFIG_SVC
 }

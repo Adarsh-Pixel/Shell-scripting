b68a26edb267e4ae8fa962f2f02c00320e5197f9
Adarsh <kumaradarsh921@gmail.com>
2023-10-09 19:45:25 +0530

    Autopush

--- Roboshop/components/shipping.sh
+++ Roboshop/components/shipping.sh
@@ -4,6 +4,6 @@ COMPONENT=shipping
 
 #This is how we import the function that was declared in a different file using source
 source components/common.sh
-NODEJS
+JAVA
 
 echo -e "\e[35m ${COMPONENT} installation is completed \e[0m \n"

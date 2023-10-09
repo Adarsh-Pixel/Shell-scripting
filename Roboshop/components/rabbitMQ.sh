b0832e523b6ca868ebe69e6ae643fecc6f264c1c
Adarsh <kumaradarsh921@gmail.com>
2023-10-09 20:11:44 +0530

    Autopush

--- Roboshop/components/rabbitMQ.sh
+++ Roboshop/components/rabbitMQ.sh
@@ -1,3 +1,34 @@
 #!/bin/bash
 
-echo "I am rabbitMQ"
+COMPONENT=rabbitmq
+
+source components/common.sh
+
+echo -e "\e[35m Congiguring ${COMPONENT}.....! \e[0m \n"
+
+echo -n "configuring ${COMPONENT} repo : "
+curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | bash
+curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | sudo bash
+stat $?
+
+echo -n "installing ${COMPONENT}  :"
+yum install rabbitmq-server -y     &>> ${LOGFILE}
+stat $?
+
+
+echo -n "starting the ${COMPONENT}  :"
+systemctl enable rabbitmq-server         &>> ${LOGFILE}
+systemctl start rabbitmq-server         &>> ${LOGFILE}
+stat $?
+
+echo -n "Creating ${COMPONENT} user account :"
+rabbitmqctl add_user roboshop roboshop123    &>> ${LOGFILE}
+stat $?
+
+echo -n "Configuring the permissions :"
+rabbitmqctl set_user_tags roboshop administrator
+rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"
+stat $?
+
+
+echo -e "\e[35m ${COMPONENT} installation is completed \e[0m"

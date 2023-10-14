f55d2ae176160dffa5beaef677b33b580c4755b8
Adarsh <kumaradarsh921@gmail.com>
2023-10-14 14:28:10 +0530

    Autopush

--- Roboshop/launch-ec2.sh
+++ Roboshop/launch-ec2.sh
@@ -17,10 +17,14 @@ fi
 AMI_ID="$(aws ec2 describe-images --filters "Name=name,Values=DevOps-LabImage-CentOS7" | jq ".Images[].ImageId" | sed -e 's/"//g')"
 SG_ID="$(aws ec2 describe-security-groups --filters Name=group-name,Values=b55-allow-all | jq ".SecurityGroups[].GroupId" | sed -e 's/"//g')"
 INSTANCE_TYPE="t2.micro"
+Hosted_Zone_ID="Z0352998G9DBZC0H61N5"
 
 echo -e "***** CREATING \e35m ${COMPONENT} \e[0m server is in progress *****"
 PRIVATEIP=$(aws ec2 run-instances --image-id ${AMI_ID} --instance-type ${INSTANCE_TYPE}  --security-group-ids ${SG_ID} --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" | jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g')
 
-echo -e "Private IP Address of $COMPONENT is $PRIVATEIP"
+echo -e "Private IP Address of $COMPONENT is $PRIVATEIP \n\n"
 
 #Each & every resource we create in Ent will have tags
+sed -e "s/COMPONENT/${COMPONENT}/" -e "s/IPADDRESS/${PRIVATEIP}/" route53.jason > /tmp/r53.jason 
+aws route53 change-resource-record-sets --hosted-zone-id $Hosted_Zone_ID --change-batch file:///tmp/r53.jason 
+echo -e "Private IP address of the $COMPONENT is created and ready to use on ${COMPONENT}.roboshop.internal"

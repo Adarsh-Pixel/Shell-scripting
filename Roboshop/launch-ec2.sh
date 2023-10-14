fec1e10f8f252315c34ac51b88c33dbf01b911d9
Adarsh <kumaradarsh921@gmail.com>
2023-10-14 12:23:24 +0530

    Autopush

--- Roboshop/launch-ec2.sh
+++ Roboshop/launch-ec2.sh
@@ -7,6 +7,13 @@
 # 4) Instances you need
 # 5) DNS_Record : Hosted_Zone_ID
 
+COMPONENT=$1
+if [ -z $1 ]; then
+    echo -e "\e[31m Component name is needed \e[0m \n \t \t"
+    echo -e "\e[35m Ex usage \e[0m \n\t\t $ bash launch ec2 "
+    exit 1
+fi 
+
 AMI_ID="ami-0c1d144c8fdd8d690"
 INSTANCE_TYPE="t2.micro"
 SG_ID="sg-03e9f6f3171da279d"

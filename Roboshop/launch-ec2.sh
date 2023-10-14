04ae95ddfafa115eff1d4e04280402730468aff7
Adarsh <kumaradarsh921@gmail.com>
2023-10-14 13:23:39 +0530

    Autopush

--- Roboshop/launch-ec2.sh
+++ Roboshop/launch-ec2.sh
@@ -14,9 +14,12 @@ if [ -z $1 ]; then
     exit 1
 fi 
 
-AMI_ID="ami-0c1d144c8fdd8d690"
+# AMI_ID="ami-0c1d144c8fdd8d690"
+AMI_ID="$(aws ec2 describe-images --filters "Name=name,Values=DevOps-LabImage-CentOS7" | jq ".Images[].ImageId" | sed -e 's/"//g')"
 INSTANCE_TYPE="t2.micro"
 SG_ID="sg-03e9f6f3171da279d"
+
+echo -e "***** CREATING \e35m ${COMPONENT} \e[0m server is in progress *****"
 PRIVATEIP=$(aws ec2 run-instances --image-id ${AMI_ID} --instance-type ${INSTANCE_TYPE}  --security-group-ids ${SG_ID} --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" | jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g')
 
 echo -e "Private IP Address of $COMPONENT is $PRIVATEIP"

32415c0be0153f249d7647050782a2d055eb80e9
Adarsh <kumaradarsh921@gmail.com>
2023-10-14 12:17:23 +0530

    Autopush

---
+++ Roboshop/launch-ec2.sh
@@ -0,0 +1,18 @@
+#!/bin/bash 
+
+# To create a server what all information needed: 
+# 1) AMI ID
+# 2) Type of Instance
+# 3) Security Group
+# 4) Instances you need
+# 5) DNS_Record : Hosted_Zone_ID
+
+AMI_ID="ami-0c1d144c8fdd8d690"
+INSTANCE_TYPE="t2.micro"
+SG_ID="sg-03e9f6f3171da279d"
+
+aws ec2 run-instances --image-id ${AMI_ID} --instance-type {INSTANCE_TYPE}  --security-group-ids ${SG_ID} 
+
+--tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]' 
+
+#Each & every resource we create in Ent will have tags

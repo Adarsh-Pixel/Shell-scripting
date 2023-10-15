8ce5df5b17c8f917bed4225dd72167dab8318939
Adarsh <kumaradarsh921@gmail.com>
2023-10-15 18:09:40 +0530

    Autopush

--- Roboshop/launch-ec2.sh
+++ Roboshop/launch-ec2.sh
@@ -1,11 +1,12 @@
 #!/bin/bash 
 
 COMPONENT=$1
+ENV=$2
 INSTANCE_TYPE="t2.micro"
 Hosted_Zone_ID="Z0352998G9DBZC0H61N5"
 
 
-if [ -z $1 ]; then
+if [ -z $1 ] || [ -z $2 ]; then
     echo -e "\e[31m Component name is needed \e[0m \n \t \t"
     echo -e "\e[35m Ex usage \e[0m \n\t\t $ bash launch ec2 "
     exit 1
@@ -16,12 +17,12 @@ SG_ID="$(aws ec2 describe-security-groups --filters Name=group-name,Values=b55-a
 
 create_ec2() {
 echo -e "***** CREATING \e35m ${COMPONENT} \e[0m server is in progress *****"
-PRIVATEIP=$(aws ec2 run-instances --image-id ${AMI_ID} --instance-type ${INSTANCE_TYPE}  --security-group-ids ${SG_ID} --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" | jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g')
+PRIVATEIP=$(aws ec2 run-instances --image-id ${AMI_ID} --instance-type ${INSTANCE_TYPE}  --security-group-ids ${SG_ID} --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}-${EMV}}]" | jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g')
 
-echo -e "Private IP Address of $COMPONENT is $PRIVATEIP \n\n"
+echo -e "Private IP Address of $COMPONENT -${EMV} is $PRIVATEIP \n\n"
 
 #Each & every resource we create in Ent will have tags
-sed -e "s/COMPONENT/${COMPONENT}/" -e "s/IPADDRESS/${PRIVATEIP}/" route53.json > /tmp/r53.json 
+sed -e "s/COMPONENT/${COMPONENT}-${EMV}/" -e "s/IPADDRESS/${PRIVATEIP}/" route53.json > /tmp/r53.json 
 aws route53 change-resource-record-sets --hosted-zone-id $Hosted_Zone_ID --change-batch file:///tmp/r53.json 
 echo -e "Private IP address of the $COMPONENT is created and ready to use on ${COMPONENT}.roboshop.internal"
 
